cd242c0c172c9d63402407b8d9cdf756b9a30d26
analauramoyano <40239185+analauramoyano@users.noreply.github.com>
2020-06-14 00:12:45 -0300

    Agregamos boletines

---
+++ boletines/javascript/main.js
@@ -0,0 +1,30 @@
+
+//DOM
+/*
+let links = document.querySelectorAll("a");
+links.forEach(function(link){
+    console.log(link);
+});
+*/
+
+/*
+let celdas = document.querySelectorAll("td");
+celdas.forEach(function(td){
+    td.addEventListener('click',function(){
+        console.log(this);
+    });
+});*/
+
+
+//Obtener los elementos de la clase .close
+/*let links = document.querySelectorAll(".close");
+links.forEach(function(link){
+    link.addEventListener("click",function(ev){
+        ev.preventDefault();
+        return false;
+    });
+});*/
+
+//Recorrerlos
+//Agregar un evento click a cada uno de ellos
+
